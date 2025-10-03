82f12acc6826d826d759249eb7bfc43d9013ad8e
Shatou136 <aishatoumohammed>
2025-10-03 17:41:02 +0100

    second push

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./db";
 import { env } from "./env";
+import { emailOTP } from "better-auth/plugins";
+import { resend } from "./resend";
 
 export const auth = betterAuth({
 database: prismaAdapter(prisma, {
@@ -11,6 +13,19 @@ database: prismaAdapter(prisma, {
         github: {
         clientId: env.AUTH_GITHUB_CLIENT_ID,
         clientSecret: env.AUTH_GITHUB_SECRET,
-        }
-    }
+        },
+    },
+
+    plugins: [
+        emailOTP({
+            async sendVerificationOTP({email, otp}) {
+            await resend.emails.send({
+    from: "ShatouLMS <onboarding@resend.dev>",
+    to: [email],
+    subject: "ShatouLMS - Verify your email",
+   html: `<p>Your OTP is <strong>${otp}</strong></p>`,
+  });
+            },
+        }),
+    ],
 });
