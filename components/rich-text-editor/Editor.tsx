3be3d87132834ce1e1351d88b86570dfaa065bb6
Shatou136 <aishatoumohammed136>
2026-01-19 13:47:20 +0100

    v6-finshed landing page

--- components/rich-text-editor/Editor.tsx
+++ components/rich-text-editor/Editor.tsx
@@ -7,18 +7,21 @@ import TextAlign from "@tiptap/extension-text-align";
 
 export function RichTextEditor({field}: {field: any}) {
     const editor = useEditor({ 
-        extensions: [StarterKit,
+        extensions: [
+            StarterKit,
             TextAlign.configure({
                 types: ["heading", "paragraph"],
             })
         ],
-        immediatelyRender: false,
+      
 
         editorProps: {
             attributes: {
                 class: "min-h-[300px] p-4 focus:outline-none prose prose-sm sm:prose lg:prose-lg xl:prose-xl dark:prose-invert !w-full !max-w-none",
             },
         },
+
+         immediatelyRender: false,
 
         onUpdate: ({editor}) => {
             field.onChange(JSON.stringify(editor.getJSON()));
